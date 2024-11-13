73c4d74aaddeeee21a9dfa97ee3c20a746697bb7
danXignat <dan.ignat@student.unitbv.ro>
2024-11-14 00:10:15 +0200

    explosion

--- Eter/Eter/ExplosionCard.cpp
+++ Eter/Eter/ExplosionCard.cpp
@@ -1,21 +1,40 @@
-#include <ranges>
 module ExplosionCard;
 
+import <format>;
+import <sstream>;
+
+import Logger;
+import Teams;
+
 namespace base {
+	using utils::Coord;
+	using namespace logger;
 
-	Explosion::Explosion(uint16_t size):
+	Explosion::Explosion(uint16_t size) :
 		m_board_size{ size },
-		m_generator{ m_random() },
-		m_effect_count{ _generateEffectCount() } {
+		m_generator{ m_random() } {
 
-		m_effects.reserve(m_effect_count);
+		m_effect_count = _generateEffectCount();
+		std::vector<Coord> positions { _generateEffectPos() };
+		std::vector<Effect> effects { _generateEffectType() };
 
-		auto positions = _generateEffectPos();
-		auto effects = _generateEffectType();
-
-		for (uint16_t i = 0; i < positions.size(); i++) {
-			m_effects.emplace(positions[i], effects[i]);
+		m_effects.resize(m_board_size);
+		for (auto& row : m_effects) {
+			row.resize(m_board_size);
 		}
+
+		for (uint16_t i = 0; i < m_effect_count; ++i) {
+			auto [row, col] = positions[i];
+
+			m_effects[row][col] = effects[i];
+
+			Logger::log(
+				Level::INFO, "explosion effect [{}, {}] -> {}",
+				row, col,
+				static_cast<int>(effects[i])
+			);
+		}
+
 	}
 
 	uint16_t Explosion::_generateEffectCount() {
@@ -26,24 +45,22 @@ namespace base {
 		return random(m_generator);
 	}
 
-	std::vector<Coord>& Explosion::_generateEffectPos() {
+	std::vector<Coord> Explosion::_generateEffectPos() {
 		std::vector<Coord> positions;
 		positions.reserve(m_board_size * m_board_size);
 
-		for (uint16_t i = 0; i < m_board_size; i++) {
-			for (uint16_t j = 0; j < m_board_size; j++) {
-				positions.emplace_back(i, j);
+		for (uint16_t i = 0; i < m_board_size; ++i) {
+			for (uint16_t j = 0; j < m_board_size; ++j) {
+				positions.push_back({ i, j });
 			}
 		}
 
 		std::shuffle(positions.begin(), positions.end(), m_generator);
 
-		std::vector<Coord> res{ positions.begin(), positions.begin() + m_board_size };
-
-		return res;
+		return { positions.begin(), positions.begin() + m_effect_count };
 	}
 
-	std::vector<Effect>& Explosion::_generateEffectType() {
+	std::vector<Effect> Explosion::_generateEffectType() {
 		std::uniform_int_distribution<uint16_t> dist{ 0, 9 };
 		std::vector<Effect> effects;
 		effects.reserve(m_effect_count);
@@ -62,6 +79,63 @@ namespace base {
 		}
 
 		return effects;
+	}
+
+	void Explosion::render() {
+		/*utils::printAtCoordinate("Explosion:", print_point.first - 2, print_point.second - 1);*/
+		Coord print_point{26, 2};
+
+		std::cout << teams::to_string(teams::Team::RED);
+
+		for (int16_t i = 0; i < m_board_size; ++i) {
+			for (int16_t j = 0; j < m_board_size; ++j) {
+				if (m_effects[i][j]) {
+					utils::printAtCoordinate(
+						std::format("{}{}",
+						static_cast<int>(m_effects[i][j].value()), ' '),
+						print_point.first + 2 * j, print_point.second + i
+					);
+				}
+				else {
+					utils::printAtCoordinate(
+						"- ",
+						print_point.first + 2 * j, print_point.second + i
+					);
+				}
+			}
+		}
+
+		std::cout << teams::to_string(teams::Team::DEFAULT);
+	}
+
+	void Explosion::rotateLeft() {
+		uint16_t size = m_board_size;
+		auto& mat = m_effects;
+
+		for (uint16_t i = 0; i < size / 2; ++i) {
+			for (uint16_t j = i; j < size - i - 1; ++j) {
+				auto temp = mat[i][j];
+				mat[i][j] = mat[j][size - i - 1];
+				mat[j][size - i - 1] = mat[size - i - 1][size - j - 1];
+				mat[size - i - 1][size - j - 1] = mat[size - j - 1][i];
+				mat[size - j - 1][i] = temp;
+			}
+		}
+	}
+
+	void Explosion::rotateRight() {
+		uint16_t size = m_board_size;
+		auto& mat = m_effects;
+
+		for (uint16_t i = 0; i < size / 2; ++i) {
+			for (uint16_t j = i; j < size - i - 1; ++j) {
+				auto temp = mat[i][j];
+				mat[i][j] = mat[size - j - 1][i];//mat[j][size - i - 1]
+				mat[size - j - 1][i] = mat[size - i - 1][size - j - 1];
+				mat[size - i - 1][size - j - 1] = mat[j][size - i - 1];
+				mat[j][size - i - 1] = temp;
+			}
+		}
 	}
 
 }
--- Eter/Eter/utils.cppm
+++ Eter/Eter/utils.cppm
@@ -7,8 +7,27 @@ import <unordered_map>;
 import CombatCard;
 
 namespace utils {
+	export using Coord = std::pair<int16_t, int16_t>;
+
+	export struct CoordFunctor {
+		size_t operator()(const Coord& coord) const {
+			std::hash<uint16_t> hasher;
+
+			return hasher(coord.first) ^ hasher(coord.second << 1);
+		}
+	};
+
+	Coord operator+(const Coord& lhs, const Coord& rhs) {
+		return { lhs.first + rhs.first, lhs.second + rhs.second };
+	}
+
 	export void printAtCoordinate(std::string_view content, uint16_t x = 0, uint16_t y = 10) {
 		std::cout << "\033[" << y << ";" << x << "H" << content;
+		std::cout << "\033[" << 10 << ";" << 0 << "H" << std::flush;
+	}
+
+	export void printAtCoordinate(std::string_view content, Coord coord) {
+		std::cout << "\033[" << coord.second << ";" << coord.first << "H" << content;
 		std::cout << "\033[" << 10 << ";" << 0 << "H" << std::flush;
 	}
 
@@ -30,4 +49,5 @@ namespace utils {
 			return false;
 		}
 	}
+
 }
