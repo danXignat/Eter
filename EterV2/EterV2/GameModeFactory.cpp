3d3f1194f713bff51fdf4e8c5c6feaba47a49026
Stefania <stefania.ionita@student.unitbv.ro>
2025-01-17 19:27:26 +0200

    timed mode done

--- EterV2/EterV2/GameModeFactory.cpp
+++ EterV2/EterV2/GameModeFactory.cpp
@@ -3,42 +3,52 @@
 #include "ElementalMode.h"
 
 namespace base {
-    GameModePtr GameModeFactory::get(const std::string& id, const std::pair<std::string, std::string>& player_names, int seconds_limit) {
+    GameModePtr GameModeFactory::get(const std::string& id, const std::pair<std::string, std::string>& player_names, int time_limit_seconds) {
         auto services = GameModeFactory::getServiceConfig(id);
-        auto primary_mode = static_cast<GameModeType>(id[0] - '0');
 
-        // pentru TimedMode (5)
-        if (primary_mode == GameModeType::TimedMode && id.length() > 1) {
-            std::unique_ptr<BaseGameMode> base_mode;
+        //  pt TimedMode (5) 
+        if (id.length() > 1 && static_cast<GameModeType>(id[0] - '0') == GameModeType::TimedMode) {
             auto secondary_mode = static_cast<GameModeType>(id[1] - '0');
+            std::unique_ptr<BaseGameMode> base_mode;
 
             switch (secondary_mode) {
-            case GameModeType::MageMode: // 2
+            case GameModeType::TrainingMode:
+                base_mode = std::make_unique<TrainingMode>(services, player_names);
+                break;
+
+            case GameModeType::MageMode:
                 base_mode = std::make_unique<MageMode>(services, player_names);
                 break;
-            case GameModeType::ElementalPowerMode: // 3
+
+            case GameModeType::ElementalPowerMode:
                 base_mode = std::make_unique<ElementalMode>(services, player_names);
                 break;
-            case GameModeType::TrainingMode: // 1
+
             default:
+                //training mode default
                 base_mode = std::make_unique<TrainingMode>(services, player_names);
                 break;
             }
-            return std::make_unique<TimedMode>(services, player_names, seconds_limit, std::move(base_mode));
+
+            return std::make_unique<TimedMode>(services, player_names, time_limit_seconds, std::move(base_mode));
         }
 
-        // Pentru moduri non-timed
-        switch (primary_mode) {
+        
+        switch (static_cast<GameModeType>(id[0] - '0')) {
         case GameModeType::TrainingMode:
             return std::make_unique<TrainingMode>(services, player_names);
+
         case GameModeType::MageMode:
             return std::make_unique<MageMode>(services, player_names);
+
         case GameModeType::ElementalPowerMode:
             return std::make_unique<ElementalMode>(services, player_names);
+
         default:
             return nullptr;
         }
     }
+
 
     GameModeType GameModeFactory::getGameModeType(const std::string& id) {
         return static_cast<GameModeType>(id.front() - '0');
