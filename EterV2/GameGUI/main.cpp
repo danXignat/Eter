2f51c903638cde2fb44bed5de05254d2cf3a960b
Sebica004 <eusebiu.cojocaru@student.unitbv.ro>
2025-01-19 22:38:55 +0200

    cursur skin

--- EterV2/GameGUI/main.cpp
+++ EterV2/GameGUI/main.cpp
@@ -9,7 +9,11 @@
 int main(int argc, char *argv[])
 {
     QApplication app{ argc, argv };
-    
+    QPixmap cursorPixmap("../pictures/estetics/custom_cursor.png");
+
+    cursorPixmap = cursorPixmap.scaled(32, 32, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    QCursor customCursor(cursorPixmap, 0, 0);
+    QApplication::setOverrideCursor(customCursor);
     base::Config::getInstance()
         .setStartPoint({ WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 })
         .setCardSpacingX(CARD_SIZE + CARD_OFFSET)
